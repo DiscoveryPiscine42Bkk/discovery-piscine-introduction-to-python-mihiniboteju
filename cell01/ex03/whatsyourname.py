4afe25bf102be104f14ea3592c038f725f7566ae
Mihini Boteju <mihiniboteju@gmail.com>
2025-06-25 21:26:03 +0530

    my cell1 ex03

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+fname = input("Hey, what's your first name? : ").strip()
+lname = input("And your last name? : ").strip()
+whole_name = fname + " " + lname
+print("Well, " + "pleased to meet you, "+ whole_name+ ".")
